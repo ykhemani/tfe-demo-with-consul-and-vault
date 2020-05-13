b109770f1f60064edf02c9bfe11699a71fd0da5a
Yash <yash@khemani.com>
2020-05-13 17:34:21 +0000

    add generic vault secret

--- 08_outputs.tf
+++ 08_outputs.tf
@@ -1,3 +1,7 @@
 output aws_instance {
   value = aws_instance.aws_instance_from_consul_data.public_ip
 }
+
+output app3-api_token {
+  value = data.vault_generic_secret.app3_api_token.data["api-token"]
+}
---
+++ 08_vault_secrets.tf
@@ -0,0 +1,3 @@
+data "vault_generic_secret" "app3_api_token" {
+  path = "kv/fin/web/app3"
+}
